e61665b923eb265deea01ead3e6418b67cfdf28c
chsassi <christian.sassii@gmail.com>
2026-01-25 19:44:19 +0100

    SEO adjustments

---
+++ src/scripts/sitemap.xml.ts
@@ -0,0 +1,40 @@
+import type { APIRoute } from "astro";
+import artists from "../data/artists.json";
+
+const SITE = "https://protocolzero.online";
+
+export const GET: APIRoute = async () => {
+  const staticPages = [
+    "/",
+    "/about",
+    "/artists",
+    "/contacts",
+    "/releases",
+    "/shop",
+  ];
+
+  const artistPages = (artists as any[])
+    .filter((artist) => typeof artist.slug === "string")
+    .map((artist) => `/artists/${artist.slug}`);
+
+  const urls = [...staticPages, ...artistPages];
+
+  const body = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls
+  .map(
+    (path) => `  <url>
+    <loc>${SITE}${path}</loc>
+  </url>`
+  )
+  .join("\n")}
+</urlset>
+`;
+
+  return new Response(body, {
+    headers: {
+      "Content-Type": "application/xml; charset=utf-8",
+      "Cache-Control": "public, max-age=3600",
+    },
+  });
+};
